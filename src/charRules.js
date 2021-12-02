9b19bc1225e57d1bcc46b4af88f00288f1864a7d
Nancy Yi Liang <hi@nyl.io>
2021-12-01 17:02:47 -0800

    closed #11: so many characters!

--- src/charRules.js
+++ src/charRules.js
@@ -2,19 +2,19 @@ const charRules = {
 	for: /.*/,
 	do: [
 		{ if: /[0-9]/, area: 'strFull'},
-		{ if: /(n|k|l|h|u|i|o|c|U)/, area: 'rhFull'},
+		{ if: /(n|ki|k|lo|l|h|u|i|o|c|U)/, area: 'rhFull'},
 		{ if: /\(\d.*\)/, 
 			for: /.*/,
 			do: [
 				{ if: /[0-9]/, area: 'strHalf'},
-				{ if: /(n|k|l|h|u|i|o|c|U)/, area: 'rhHalf'},
+				{ if: /(n|ki|k|lo|h|u|i|o|c|U)/, area: 'rhHalf'},
 			]
 		},
 		{ if: /(s|d|v|f|x)/, area: 'lhHalf',
 			for: /.*/,
 			do: [
 				{ if: /[0-9]/, area: 'strHalf'},
-				{ if: /(n|k|l|h|u|i|o|c|U)/, area: 'rhHalf'},
+				{ if: /(n|ki|k|lo|l|h|u|i|o|c|U)/, area: 'rhHalf'},
 			]
 		},
 		{ if: /\(\d+\)/,
@@ -37,11 +37,11 @@ const charRules = {
 				{ if: /\,\d.*/, remove: [','], area: 'strHalfBottom' },
 			]
 		},
-		{ if: /j|V/, area: 'thirdMidFull', 
+		{ if: /j|V|滚沸|滚|沸|到/, area: 'thirdMidFull', 
 			for: /.*/,
 			do: [
 				{ if: /[0-9]/, area: 'strThird'},
-				{ if: /(n|k|l|h|u|i|o|c)/, area: 'rhThirdBottom'}
+				{ if: /(n|ki|k|lo|l|h|u|i|o|c)/, area: 'rhThirdBottom'}
 			]
 		},
 		{ if: /\(0\)/, 
@@ -52,8 +52,8 @@ const charRules = {
 		},
 		{ if: /\/|\>/, area: 'halfUpper' },
 		{ if: /\\/, area: 'halfLeft'  },
-		{ if: /H|Y|I|O/, area: 'cuo', 
-			for: /[^H|Y|I|O]*/i,
+		{ if: /H|Y|I|O|TTT|TT|BL|B|L/, area: 'cuo', 
+			for: /[^H|Y|I|O|TTT|TT|BL|B|L]*/i,
 			do: [
 				{ if: /[0-9]/, area: 'strHalf' },
 				{ if: /(s|d|v|f|x)|(\([0-9].*\))/,
@@ -84,8 +84,8 @@ const charRules = {
 				}
 			]
 		},
-		{ if: /H|Y|I|O/, area: 'cuo', 
-			for: /[H|Y|I|O](.*)/,
+		{ if: /H|Y|I|O|TTT|TT|BL|B|L/, area: 'cuo', 
+			for: /[H|Y|I|O|TTT|TT|BL|B|L](.*)/,
 			do: [
 				{ if: /[0-9]/, area: 'strHalf' },
 				{ if: /(s|d|v|f|x)|(\([0-9].*\))/,
@@ -118,12 +118,18 @@ const charRules = {
 		},
 		{ if: /退/, area: 'chineseChar' },
 		{ if: /长吟|長吟/, use: '长吟', area: 'chineseChar' },
+		{ if: /细吟/, area: 'chineseChar' },
 		{ if: /进|進/, use: '进', area: 'chineseChar' },
 		{ if: /分开/, area: 'chineseChar' },
 		{ if: /泛止/, area: 'chineseChar' },
 		{ if: /泛起/, area: 'chineseChar' },
 		{ if: /大息/, area: 'chineseChar' },
 		{ if: /少息/, area: 'chineseChar' },
-		{ if: /复/, area: 'chineseChar' }
+		{ if: /复/, area: 'chineseChar' },
+		{ if: /带起/, area: 'chineseChar' },
+		{ if: /对起/, area: 'chineseChar' },
+		{ if: /爪起/, area: 'chineseChar' },
+		{ if: /撇起/, area: 'chineseChar' },
+		{ if: /起/, area: 'chineseChar' }
 	]
 }
